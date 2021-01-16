38faacf9ac95da082b8a0d5cf99d9382b7c7f4c2
Ваше Имя <you@example.com>
2021-01-16 14:33:21 +0300

    Added site name to title

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const config = require('../config.js');
 
 function index(req, res) {
-    res.render('index', {title: 'Главная'});
+    res.render('index', {title: 'Главная', config: config});
 };
 
 module.exports.index = index;
